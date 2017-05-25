b5808d2c13d60f15a987488fd67c4bc333b72744
Daniel Lennox <dlennox2424@gmail.com>
2017-05-25 14:05:23 -0600

    Adding csu-app-template

--- src/components/App.js
+++ src/components/App.js
@@ -1,66 +1,16 @@
 import React, {
   Component
 } from 'react';
-import $ from 'jquery';
-import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import {
-  Toolbar,
-  ToolbarTitle,
-  ToolbarGroup,
-  ToolbarSeparator
-} from 'material-ui/Toolbar';
-import FontIcon from 'material-ui/FontIcon';
-
-import CsuSvgLogo, {
-  CsuFooter
-} from './CsuBranding';
+import AppContainer from '../csu-app-template/AppContainer';
 import InputForm from '../containers/InputForm';
 import OutputSignature from '../containers/OutputSignature';
 
 import config from '../config.json';
-import './App.css';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../../node_modules/font-awesome/css/font-awesome.min.css';
-
-document.title = document.title === '' ? config.appName + ' - ' + config.unitTitle : document.title;
 
 class App extends Component {
-  componentDidMount() {
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 1) {
-        $('#logobar').addClass('display-none');
-        $('#top-toolbar').addClass('sticky');
-      } else {
-        $('#logobar').removeClass('display-none');
-        $('#top-toolbar').removeClass('sticky');
-      }
-    });
-  }
   render() {
     return (
-      <div>
-        <AppBar
-          id='logobar'
-          iconElementLeft={<CsuSvgLogo />} />
-        <Toolbar id='top-toolbar'>
-          <ToolbarTitle text={config.appName} />
-          <ToolbarGroup>
-            <IconButton
-              href='/'
-              tooltip='Home'>
-              <FontIcon className='material-icons'>home</FontIcon>
-            </IconButton>
-            <ToolbarSeparator style={{marginLeft:0}} />
-            <IconButton
-              href={config.unitContact}
-              tooltip='Contact Us'
-              tooltipPosition='bottom-left'>
-              <FontIcon className='material-icons'>email</FontIcon>
-            </IconButton>
-          </ToolbarGroup>
-        </Toolbar>
-        <div id="main-content">
+      <AppContainer config={config}>
           <div className='row'>
             <div className='col-md-7'>
               <InputForm />
@@ -69,9 +19,7 @@ class App extends Component {
               <OutputSignature />
             </div>
           </div>
-        </div>
-        <CsuFooter/>
-      </div>
+      </AppContainer>
     );
   }
 }
